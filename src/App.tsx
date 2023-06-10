6cf5fcf09dacca50f08fd7aff2beee100680e939
sleychez <v0401podtyagin@mail.ru>
2023-06-11 01:25:11 +0300

    delete navbar and add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Route} from "react-router-dom";
+import Header from './components/Header/Header';
+import {BrowserRouter} from "react-router-dom";
 import Todos from './components/Todos/Todos';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Navbar/>
-      <Route exact path={'/'} component={Todos}/>
+      <Header/>
+      <Todos/>
     </BrowserRouter>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header: React.FC = () => (
+  <nav className='header purple darken-1'>
+    <div className="nav-wrapper">
+      <span className="brand-logo">React + Typescript Todo</span>
+    </div>
+  </nav>
+)
+
+export default Header
