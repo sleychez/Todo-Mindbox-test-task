adc822d90f1fa1295200f7a4b7e48174adae7239
sleychez <v0401podtyagin@mail.ru>
2023-06-11 01:36:53 +0300

    deleted react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import {BrowserRouter} from "react-router-dom";
 import Todos from './components/Todos/Todos';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <>
       <Header/>
       <Todos/>
-    </BrowserRouter>
+    </>
   );
 }
 
